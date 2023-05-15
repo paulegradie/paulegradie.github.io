67edbff213e21092e3d001f16781c0ba056f99b7
Paul Gradie <paul.e.gradie@gmail.com>
2023-05-15 23:45:53 +1000

    config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 import nextMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 import rehypePrism from '@mapbox/rehype-prism'
+const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx'],
   reactStrictMode: true,
-  // images: {
-  //   loader: "akamai",
-  //   path: "",
-  // },
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 }
 
 const withMDX = nextMDX({
