ae4ed562c8b2fe19d616d88875a7422ed9f73483
Paul Gradie <paul.e.gradie@gmail.com>
2023-05-15 23:22:33 +1000

    udpates to config and packages

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,6 @@ import rehypePrism from '@mapbox/rehype-prism'
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx'],
   reactStrictMode: true,
-  experimental: {
-    scrollRestoration: true,
-  },
   images: {
     loader: "akamai",
     path: "",
