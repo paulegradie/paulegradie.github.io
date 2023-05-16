17c845d720042cadd4faae96e4915d111d99d8d5
Paul Gradie <paul.e.gradie@gmail.com>
2023-05-16 18:05:32 +1000

    test

--- next.config.mjs
+++ next.config.mjs
@@ -4,21 +4,29 @@ import rehypePrism from '@mapbox/rehype-prism'
 const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+let nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx'],
   reactStrictMode: true,
-  experimental: {
-    mdxRs: true,
-  },
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
 }
+
+if (isProd) {
+  nextConfig = {
+    ...nextConfig,
+    experimental: {
+      mdxRs: true,
+    },
+    images: {
+      loader: 'akamai',
+      path: '',
+    },
+  }
+}
+
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
   options: {
+    providerImportSource: "@mdx-js/react",
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypePrism],
   },
