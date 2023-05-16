3c677ee4f4393ed1447dc5993356b795e3971c91
Paul Gradie <paul.e.gradie@gmail.com>
2023-05-16 17:53:28 +1000

    test

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx'],
   reactStrictMode: true,
+  future: { webpack5: true },
   images: {
     loader: 'akamai',
     path: '',
