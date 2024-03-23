c119e45988cb61abd158040d92afd1afe13c1e75
Paul Gradie <paul.e.gradie@gmail.com>
2024-03-23 20:04:35 +1100

    update about me

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -42,45 +42,31 @@ export default function About() {
                     </div>
                     <div className="lg:order-first lg:row-span-2">
                         <h1 className="text-4xl font-bold tracking-tight text-zinc-300 dark:text-zinc-100 sm:text-4xl">
-                            I`m Paul Gradie.
+                            Hi - I’m Paul Gradie.
                         </h1>
-                        <h1 className="text-2xl font-bold tracking-tight text-zinc-400 dark:text-zinc-100 sm:text-2xl">
-                            Here is the short version of my story.
-                        </h1>
+                        <h2 className="text-2xl font-bold tracking-tight text-zinc-400 dark:text-zinc-100 sm:text-2xl">
+                            So about me.
+                        </h2>
                         <div className="mt-6 space-y-7 text-base text-zinc-400 dark:text-zinc-400 text-justify">
                             <p>
-                                It all began with a deep passion for music, which began with tapping on my dad&apos;s guitar case with wooden spoons. I dreamed of becoming a studio drummer, envisioning myself playing alongside legendary bands like Steely Dan. Music was my world, and I even earned a audition-based scholarship to the prestigious Berklee College of Music. While my dream of becoming a drummer didn&apos;t materialize, those early musical pursuits were the first of many valuable experiences. And the first of many transitions that would ultimately give me a unique perspective on life, change, and knowledge itself.
+                                I am a software engineer working mostly in the .NET ecosystem. I help companies build and reshape software to solve business problems using iterative delivery practices. My approach is to blend being principled with being pragmatic while slowly evolving towards a longer term vision. I find that this approach not only provides better transparency to stakeholder, it also yields a better sense of progress to the team and ensure we always deliver concrete value.
                             </p>
                             <p>
-                                Following my musical aspirations, I transitioned into the field of biology. During my university years, I delved into the intricate workings of life itself, studying biology, chemistry, physics, and even conducting some research. At the time, I had aspirations of becoming a dentist.. although I can&apos;t quite pinpoint the exact reason for choosing that career path. It was more of an intuitive inclination, fueled by my enjoyment of dental visits. However, fate had other plans, and I faced rejection from dental schools - which became probably the most pivotable failure of my life.
+                                This approach is informed by experience - both from in the realm of software as well as previous disciplines that I’ve worked in.
                             </p>
                             <p>
-                                As they say - what one door closes, another opens.
+                                Before I transitioned into software engineering, I was a biologist. I have a PhD in developmental genetics from the University of Melbourne, where I employed a mixture of wet lab and computational skills to answer research questions about the develop of reproductive systems. I became deeply interested and familiar with the process of evolution during this phase of my career - which has had a substantial impact on how I understand not only change, but the risks associated with it.
                             </p>
                             <p>
-                                As chance would have it, I crossed paths with a remarkable mentor who became my PhD advisor and opened a new door setting me on a captivating journey in the realm of research on developmental genetics. Six years of passionate exploration and discovery followed, but by the end, I realized that the academic realm and traditional research paths didn&apos;t align with my aspirations. While completing my PhD was a significant accomplishment, it felt like another setback. I could not find a clear path to leverage my doctoral experience in a way that would ultimately leave me feeling satisfied with both the current state of my life, as well as my long term trajectory.
-                            </p>
-                            <p>
-                                It was during this time that I found myself drawn towards the world of Artificial Intelligence. Through a fortunate combination of timing, the skills I honed during my PhD — such as research, analytics, programming, and statistical modeling - I secured my first job at a reputable company called Zendesk. There, I immersed myself in the tech industry, working on the development of large multilingual language models and contributing to the invention of new artificially intelligent systems. The culmination of this work resulted in a co-authored patent and a large amount of industry software development experience. However, as I looked ahead, I began to recognize the transient nature of the data science field, leading me to conclude that it wasn&apos;t the long-term stable future I sought.
-                            </p>
-                            <p>
-                                That realization prompted my pivot towards software engineering, which brings me to where I am today — writing this to you. This career path feels like the most natural fit for me, as it allows me to draw upon all my previous experiences and failures to tackle complex and challenging problems. I&apos;ve embraced the diverse roles I&apos;ve held throughout my life — musician, biologist, researcher, data scientist, AI researcher, inventor — and they have collectively shaped me into the person I am now.
 
+                                I’m also interested in understanding how to effectively build strong, functional teams. Teams are just people, and people are complex. People come with infinite variation in their experience, skills, personalities, needs, triggers, motivations, and much more. I enjoy learning about people, understanding their strengths and weaknesses, and how to combine them to achieve outstanding results. I also understand that people have their limits, so I also think about how to achieve this without causing burnout.
                             </p>
                             <p>
-                                I approach life with an open mind and a wealth of experience. I&apos;m curious yet cautious, adventurous yet wise, critical yet friendly, and intelligent yet humble. These qualities, born from my journey, guide me as I navigate this fulfilling career.
+                                I’m not selective about the people I work with, but I do have high expectations for those whom I do. Why? I invest a lot into being the best that I can be. That doesn’t mean I expect people to do more than is required (like working out of hours), but it does mean that I expect people to be engaged.
                             </p>
-                            <p>I have been many things:</p>
-                            <ul className="list-none">
-                                <li className="">Musician</li>
-                                <li className="ml-3">Biologist</li>
-                                <li className="ml-6">Researcher</li>
-                                <li className="ml-9">Data Scientist / AI Researcher / Inventor</li>
-                                <li className="ml-12">Software Engineer</li>
-                            </ul>
                             <p>
-                                All of these experiences, and failures, make me who I am today. Open-minded but experienced. Curious but cautious.
-                                Adventurous but wise. Critical but friendly. Intelligent but humble.
+
+                                I hope this gives you a little bit of insight into the type of person I am. There is far more to my story, so if you’re interested to know more - don’t hesitate to get in touch.
                             </p>
                         </div>
                     </div>
